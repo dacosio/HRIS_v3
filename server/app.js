b345b712364e041818e6a1dd054d43aee097f631
Suruchi1986 <suruchis.sharma@gmail.com>
2020-03-11 09:48:57 +0530

    update employee

--- server/app.js
+++ server/app.js
@@ -1,74 +1,71 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var cors = require('cors');
-var exphbs  = require('express-handlebars');
-
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var cors = require("cors");
+var exphbs = require("express-handlebars");
 
 //routes folder import
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var departmentsRouter = require('./routes/api/department');
-var dependentsRouter = require('./routes/api/dependent');
-var emergencyContactsRouter = require('./routes/api/emergency_contact');
-var employeesRouter = require('./routes/api/employee');
-var leavesRouter = require('./routes/api/leave');
-var logsRouter = require('./routes/api/log');
-var rolesRouter = require('./routes/api/role');
-var undertimeOvertimeRouter = require('./routes/api/undertime_overtime');
-var usersRouter = require('./routes/api/user');
-var eodsRouter = require('./routes/api/eod');
-var applicantsRouter = require('./routes/api/applicant');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var departmentsRouter = require("./routes/api/department");
+var dependentsRouter = require("./routes/api/dependent");
+var emergencyContactsRouter = require("./routes/api/emergency_contact");
+var employeesRouter = require("./routes/api/employee");
+var leavesRouter = require("./routes/api/leave");
+var logsRouter = require("./routes/api/log");
+var rolesRouter = require("./routes/api/role");
+var undertimeOvertimeRouter = require("./routes/api/undertime_overtime");
+var usersRouter = require("./routes/api/user");
+var eodsRouter = require("./routes/api/eod");
+var applicantsRouter = require("./routes/api/applicant");
 
 var app = express();
 app.use(cors());
 
 // view engine setup
-app.engine('.hbs', exphbs({
-  extname: '.hbs',
-  defaultLayout: 'main',
-  helpers: {
-    ifCond: function(v1, v2, options) {
-      if(v1 === v2) {
+app.engine(
+  ".hbs",
+  exphbs({
+    extname: ".hbs",
+    defaultLayout: "main",
+    helpers: {
+      ifCond: function(v1, v2, options) {
+        if (v1 === v2) {
           return options.fn(this);
+        }
+        return options.inverse(this);
       }
-      return options.inverse(this);
     }
-  }
-}));
-app.set('view engine', '.hbs');
+  })
+);
+app.set("view engine", ".hbs");
 
 app.use(cookieParser());
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //router from routes folder
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api/departments',departmentsRouter);
-app.use('/api/dependents',dependentsRouter);
-app.use('/api/emergency',emergencyContactsRouter);
-app.use('/api/employees',employeesRouter);
-app.use('/api/leaves',leavesRouter);
-app.use('/api/logs',logsRouter);
-app.use('/api/roles',rolesRouter);
-app.use('/api/time',undertimeOvertimeRouter);
-app.use('/api/users',usersRouter);
-app.use('/api/eods',eodsRouter);
-app.use('/api/applicants',applicantsRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api/departments", departmentsRouter);
+app.use("/api/dependents", dependentsRouter);
+app.use("/api/emergency", emergencyContactsRouter);
+app.use("/api/employees", employeesRouter);
+app.use("/api/leaves", leavesRouter);
+app.use("/api/logs", logsRouter);
+app.use("/api/roles", rolesRouter);
+app.use("/api/time", undertimeOvertimeRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/eods", eodsRouter);
+app.use("/api/applicants", applicantsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -79,11 +76,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- server/routes/api/applicant.js
+++ server/routes/api/applicant.js
@@ -1,67 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const ApplicantService = require('../../services/applicant.service');
+const ApplicantService = require("../../services/applicant.service");
 
 const applicantService = new ApplicantService();
 
 /********** These are all mounted to /api/applicants *********/
 
 //get all applicants
-router.get('/', function(req,res,next) {
-    applicantService
-        .getAll()
-        .then(applicants => {
-            res.json(applicants)
-        });
+router.get("/", function(req, res, next) {
+  applicantService.getAll().then(applicants => {
+    res.json(applicants);
+  });
 });
 
-
 //get a specific applicant
-router.get('/:id', function(req,res,next) {
-    applicantService
-        .get(req.params.id)
-        .then(applicant => res.json(applicant))
+router.get("/:id", function(req, res, next) {
+  applicantService.get(req.params.id).then(applicant => res.json(applicant));
 });
 
 //create applicant
-router.post('/', function(req,res,next){
-    let applicant = {
-        date: req.body.date,
-        time: req.body.time,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        contact_no: req.body.contact_no,
-        department_id: req.body.department_id,
-        created_by: req.body.created_by //todo
-    };
-    applicantService
-        .create(applicant)
-        .then(id => res.json(id))
+router.post("/", function(req, res, next) {
+  let applicant = {
+    date: req.body.date,
+    time: req.body.time,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    contact_no: req.body.contact_no,
+    department_id: req.body.department_id,
+    created_by: req.body.created_by //todo
+  };
+  applicantService.create(applicant).then(id => res.json(id));
 });
-
 
 //update the applicant
-router.put('/:id', function(req,res,next){
-    let applicant = {
-        date: req.body.date,
-        time: req.body.time,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        contact_no: req.body.contact_no,
-        department_id: req.body.department_id,
-        created_by: req.body.created_by //todo
-    };
-    applicantService
-        .update(req.params.id,applicant)
-        .then(affected => res.json(affected))
-});
+// router.put('/:id', function(req,res,next){
+//     let applicant = {
+//         date: req.body.date,
+//         time: req.body.time,
+//         first_name: req.body.first_name,
+//         last_name: req.body.last_name,
+//         contact_no: req.body.contact_no,
+//         department_id: req.body.department_id,
+//         created_by: req.body.created_by //todo
+//     };
+//     applicantService
+//         .update(req.params.id,applicant)
+//         .then(affected => res.json(affected))
+// });
 
 //delete applicant
-router.delete('/:id', function(req,res,next){
-    applicantService
-        .delete(req.params.id)
-        .then(affected => res.json(affected))
+router.delete("/:id", function(req, res, next) {
+  applicantService.delete(req.params.id).then(affected => res.json(affected));
 });
-
 
 module.exports = router;
